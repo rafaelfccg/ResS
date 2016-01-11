786aaafcbd4c4155e58efb634a1fcf47fa2cfe02
Danielle Sales <dso@cin.ufpe.br>
2016-01-11 13:10:43 -0300

    updated AdminUserTestDataAndOperations.groovy

--- test/functional/steps/AdminUserTestDataAndOperations.groovy
+++ test/functional/steps/AdminUserTestDataAndOperations.groovy
@@ -75,7 +75,7 @@ public class AdminUserTestDataAndOperations {
         if(email != null && oldUser != null) {
             def cont = new AdminUserController()
             cont.params << newAdminUser
-            cont.edit(adminUser)
+            cont.edit()
             cont.response.reset()
         }
     }
